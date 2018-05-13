64cc7e9d6797687da3970f958cd501abaa958502
oussaka <oussaka@users.noreply.github.com>
2018-05-13 14:53:02 +0000

    composer branch alias update

--- web/app.php
+++ web/app.php
@@ -11,8 +11,8 @@ $kernel = new AppKernel('prod', false);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 // Request::enableHttpMethodParameterOverride();
-// Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
-// Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8']);
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8']);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
