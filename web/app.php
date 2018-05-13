5f1685aeddb22a2fc460adbe10ecd8fddcb36b89
oussaka <oussaka@users.noreply.github.com>
2018-05-13 14:44:35 +0000

    remove deprecation sf3.4

--- web/app.php
+++ web/app.php
@@ -7,11 +7,13 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+// Request::enableHttpMethodParameterOverride();
+// Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+// Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8']);
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
